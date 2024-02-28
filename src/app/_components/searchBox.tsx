a1b7ad8b0b261b7ec0727f79cb23f37f92803ecf
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-28 18:27:26 +0545

    created search results

--- src/app/_components/searchBox.tsx
+++ src/app/_components/searchBox.tsx
@@ -16,14 +16,13 @@ const SearchBar = () => {
     return (
         <div className="relative flex-1 flex flex-row pr-4">
             <div className="hidden sm:flex-1"></div>
-            <div className="w-[100%] sm:w-[55%] md:w-[55%] pl-2 bg-white rounded-lg relative">
+            <div  className="w-[100%] sm:w-[55%] md:w-[55%] pl-2 bg-white rounded-lg relative">
                 <input className="w-[100%] bg-transparent border-none text-black active:border-none"
                     type="text" 
                     placeholder={_searchLabel}
                     value={_searchText}
                     onChange={(e) => setSearchText(e.target.value)}
                 />
-
             </div>
             
             <div className="hidden sm:flex-1 sm:inline"></div>
@@ -32,27 +31,37 @@ const SearchBar = () => {
     )
 }
 
-const BaseBtnClass=""
-const activeBtnClass=" border-b-2 border-white hover:border-b-purple-500"
 
 
-/*Search Selector and search text box */
-const SearchTextBox = () => {
+
+const SearchSelector = () => {
     const [_searchById,setSearchById] = useAtom(searchById)
 
-    const searchSelector = (
+    return (
         <>
         <div className="pr-4">
-            <button className={"base-btn active-btn"}><p>Id</p></button>
+        <button 
+        className={"base-btn " +  (!_searchById?"active-btn":"")}
+        onClick={()=> setSearchById(_=> false)}>
+            Name
+        </button>
         </div>
-        <button className="base-btn">Name</button>
+        <button 
+            className={"base-btn " + (_searchById?"active-btn":"")}
+            onClick={() => setSearchById(_=> true)}>
+                <p>Id</p>
+            </button>
         </>
 
     )
-    return (
+}
+
+/*Search Selector and search text box */
+const SearchTextBox = () => {
+        return (
         <div className="abosolute pt-4 sticky border-none flex flex-row items-baseline">
         <SearchBar />
-            {searchSelector}
+        <SearchSelector />
         </div>
     )
 }
