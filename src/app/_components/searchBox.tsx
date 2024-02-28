f2912d3c72c073a7815babd88e03c4cac0149f9c
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-28 18:12:48 +0545

    styled search bars

--- src/app/_components/searchBox.tsx
+++ src/app/_components/searchBox.tsx
@@ -1,35 +1,58 @@
-`use client`
+'use client'
 import { useAtom,atom, useAtomValue } from "jotai"
 import { searchById, textToSearch } from "../stores/inputBoxState"
+import "./searchBox.tsx.css"
 
 
 const searchLabel = atom((get) => {
     return get(searchById)?"Search By Id":"Search By Label"
 });
 
+const SearchBar = () => {
+    const [_searchText,setSearchText] = useAtom(textToSearch)
+    const _searchLabel = useAtomValue(searchLabel)
+
+
+    return (
+        <div className="relative flex-1 flex flex-row pr-4">
+            <div className="hidden sm:flex-1"></div>
+            <div className="w-[100%] sm:w-[55%] md:w-[55%] pl-2 bg-white rounded-lg relative">
+                <input className="w-[100%] bg-transparent border-none text-black active:border-none"
+                    type="text" 
+                    placeholder={_searchLabel}
+                    value={_searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+
+            </div>
+            
+            <div className="hidden sm:flex-1 sm:inline"></div>
+
+        </div>
+    )
+}
+
+const BaseBtnClass=""
+const activeBtnClass=" border-b-2 border-white hover:border-b-purple-500"
+
 
 /*Search Selector and search text box */
 const SearchTextBox = () => {
     const [_searchById,setSearchById] = useAtom(searchById)
-    const [_searchText,setSearchText] = useAtom(textToSearch)
-    const _searchLabel = useAtomValue(searchLabel)
 
     const searchSelector = (
         <>
-        <button>Id</button>
-        <button>Name</button>
+        <div className="pr-4">
+            <button className={"base-btn active-btn"}><p>Id</p></button>
+        </div>
+        <button className="base-btn">Name</button>
         </>
 
     )
     return (
-        <div className="w-[100%] border-none flex flex-row items-baseline">
-            <input className="flex-1" 
-            type="text" 
-            placeholder={_searchLabel}
-            value={_searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-        />
-        {searchSelector}
+        <div className="abosolute pt-4 sticky border-none flex flex-row items-baseline">
+        <SearchBar />
+            {searchSelector}
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,10 @@ export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="pt-4 px-8">
         <SearchTextBox />
+      </div>
     </main>
   );
 }
